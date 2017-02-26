4920b3707e7cf36ee9f121d3871aba224f51cc40
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-02-26 14:19:59 +0000

    .

--- inc/eint_t.h
+++ inc/eint_t.h
@@ -1,7 +1,24 @@
 # ifndef __eint__t__h
 # define __eint__t__h
-# include <stdint.h>
 
+# ifdef USING_OPENCL
+	# include <CL/cl.h>
+# ifdef ARC64
+	typedef cl_ulong cl_uint_t;
+	typedef cl_long cl_int_t;
+# elif ARC32
+	typedef cl_uint cl_uint_t;
+	typedef cl_int cl_int_t;
+# elif ARC16
+	typedef cl_ushort cl_uint_t;
+	typedef cl_short cl_int_t;
+# elif ARC8
+	typedef cl_uchar cl_uint_t;
+	typedef cl_char cl_int_t;
+# endif
+# endif
+
+# include <stdint.h>
 # ifdef ARC64
     typedef uint64_t uint_t;
     typedef int64_t int_t;
