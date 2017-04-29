86b695278e9701a280694c746e1f817090c0b540
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-04-29 14:21:24 +0100

    .

--- inc/eint_t.h
+++ inc/eint_t.h
@@ -1,40 +1,48 @@
 # ifndef __mdl__eint__t__h
 # define __mdl__eint__t__h
-# if (defined(__USING_OPENCL) || defined(USING_OPENCL)) && defined(USE_CL_TYPES)
+# if (defined(__USING_OPENCL) || defined(USING_OPENCL)) && defined(__OPENCL_LNG_TYPES)
 # if defined(ARC64) || defined(__ARC64)
-	typedef ulong mdl_uint_t;
-	typedef long mdl_int_t;
+typedef ulong mdl_uint_t;
+typedef long mdl_int_t;
 # elif defined(ARC32) || defined(__ARC32)
-	typedef uint mdl_uint_t;
-	typedef int mdl_int_t;
+typedef uint mdl_uint_t;
+typedef int mdl_int_t;
 # elif defined(ARC16) || defined(__ARC16)
-	typedef ushort mdl_uint_t;
-	typedef short mdl_int_t;
+typedef ushort mdl_uint_t;
+typedef short mdl_int_t;
 # elif defined(ARC8) || defined(__ARC8)
-	typedef uchar mdl_uint_t;
-	typedef char mdl_int_t;
+typedef uchar mdl_uint_t;
+typedef char mdl_int_t;
 # endif
-# else
+typedef ulong mdl_u64_t;
+typedef long mdl_i64_t;
 
+typedef uint mdl_u32_t;
+typedef int mdl_i32_t;
+
+typedef ushort mdl_u16_t;
+typedef short mdl_i16_t;
+
+typedef uchar mdl_u8_t;
+typedef char mdl_i8_t;
+# else
 # include <stdint.h>
 # if defined(ARC64) || defined(__ARC64)
-    typedef uint64_t mdl_uint_t;
-    typedef int64_t mdl_int_t;
+typedef uint64_t mdl_uint_t;
+typedef int64_t mdl_int_t;
 # elif defined(ARC32) || defined(__ARC32)
-    typedef uint32_t mdl_uint_t;
-    typedef int32_t mdl_int_t;
+typedef uint32_t mdl_uint_t;
+typedef int32_t mdl_int_t;
 # elif defined(ARC16) || defined(__ARC16)
-    typedef uint16_t mdl_uint_t;
-    typedef int16_t mdl_int_t;
+typedef uint16_t mdl_uint_t;
+typedef int16_t mdl_int_t;
 # elif defined(ARC8) || defined(__ARC8)
-    typedef uint8_t mdl_uint_t;
-    typedef int8_t mdl_int_t;
+typedef uint8_t mdl_uint_t;
+typedef int8_t mdl_int_t;
 # else
-    typedef int unsigned mdl_uint_t;
-    typedef int mdl_int_t;
+typedef int unsigned mdl_uint_t;
+typedef int mdl_int_t;
 # endif
-# endif
-
 typedef uint64_t mdl_u64_t;
 typedef int64_t mdl_i64_t;
 
@@ -46,4 +54,19 @@ typedef int16_t mdl_i16_t;
 
 typedef uint8_t mdl_u8_t;
 typedef int8_t mdl_i8_t;
+# if (defined(__USING_OPENCL) || defined(USING_OPENCL)) && defined(__OPENCL_API_TYPES)
+# include <CL/cl.h>
+typedef cl_ulong mdl_cl_u64_t;
+typedef cl_long mdl_cl_i64_t;
+
+typedef cl_uint mdl_cl_u32_t;
+typedef cl_int mdl_cl_i32_t;
+
+typedef cl_ushort mdl_cl_u16_t;
+typedef cl_short mdl_cl_i16_t;
+
+typedef cl_uchar mdl_cl_u8_t;
+typedef cl_char mdl_cl_i8_t;
+# endif
+# endif
 # endif /*__mdl__eint__t__h*/
--- inc/eint_t.hpp
+++ inc/eint_t.hpp
@@ -3,31 +3,46 @@
 # include <boost/cstdint.hpp>
 namespace mdl {
 # if defined(ARC64) || defined(__ARC64)
-    typedef boost::uint64_t uint_t;
-    typedef boost::int64_t int_t;
+typedef boost::uint64_t uint_t;
+typedef boost::int64_t int_t;
 # elif defined(ARC32) || defined(__ARC32)
-    typedef boost::uint32_t uint_t;
-    typedef boost::int32_t int_t;
+typedef boost::uint32_t uint_t;
+typedef boost::int32_t int_t;
 # elif defined(ARC16) || defined(__ARC16)
-    typedef boost::uint16_t uint_t;
-    typedef boost::int16_t int_t;
+typedef boost::uint16_t uint_t;
+typedef boost::int16_t int_t;
 # elif defined(ARC8) || defined(__ARC8)
-    typedef boost::uint8_t uint_t;
-    typedef boost::int8_t int_t;
+typedef boost::uint8_t uint_t;
+typedef boost::int8_t int_t;
 # else
-    typedef int unsigned uint_t;
-    typedef int int_t;
+typedef int unsigned uint_t;
+typedef int int_t;
 # endif
-	typedef boost::uint64_t u64_t;
-	typedef boost::int64_t i64_t;
+typedef boost::uint64_t u64_t;
+typedef boost::int64_t i64_t;
 
-	typedef boost::uint32_t u32_t;
-	typedef boost::int32_t i32_t;
+typedef boost::uint32_t u32_t;
+typedef boost::int32_t i32_t;
 
-	typedef boost::uint16_t u16_t;
-	typedef boost::int16_t i16_t;
+typedef boost::uint16_t u16_t;
+typedef boost::int16_t i16_t;
 
-	typedef boost::uint8_t u8_t;
-	typedef boost::int8_t i8_t;
+typedef boost::uint8_t u8_t;
+typedef boost::int8_t i8_t;
+
+# if (defined(__USING_OPENCL) || defined(USING_OPENCL)) && defined(__OPENCL_API_TYPES)
+# include <CL/cl.hpp>
+typedef cl_ulong cl_u64_t;
+typedef cl_long cl_i64_t;
+
+typedef cl_uint cl_u32_t;
+typedef cl_int cl_i32_t;
+
+typedef cl_ushort cl_u16_t;
+typedef cl_short cl_i16_t;
+
+typedef cl_uchar cl_u8_t;
+typedef cl_char cl_i8_t;
+# endif
 }
 # endif /*__mdl__eint__t__hpp*/
